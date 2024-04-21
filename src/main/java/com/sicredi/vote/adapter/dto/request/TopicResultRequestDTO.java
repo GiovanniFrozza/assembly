be4f241942ee602315099c4319a45eac9435f416
giovanni <giovannifrozza470@gmail.com>
2024-04-21 10:50:06 -0300

    adjusted return status of the endpoints

--- src/main/java/com/sicredi/vote/adapter/dto/request/TopicResultRequestDTO.java
+++ src/main/java/com/sicredi/vote/adapter/dto/request/TopicResultRequestDTO.java
@@ -1,5 +1,7 @@
 package com.sicredi.vote.adapter.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Builder
@@ -8,5 +10,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TopicResultRequestDTO {
-    private String id;
+    @Schema(description = "Id of the topic", example = "6623fa79c243656c3afb2f0c")
+    @NotBlank(message = "The field [topicId] is required and cannot be empty.")
+    private String topicId;
 }
--- src/main/java/com/sicredi/vote/adapter/resource/TopicController.java
+++ src/main/java/com/sicredi/vote/adapter/resource/TopicController.java
@@ -4,8 +4,9 @@ import com.sicredi.vote.adapter.dto.request.CreateSessionRequestDTO;
 import com.sicredi.vote.adapter.dto.request.TopicRequestDTO;
 import com.sicredi.vote.adapter.dto.request.TopicResultRequestDTO;
 import com.sicredi.vote.adapter.dto.request.VoteRequestDTO;
+import com.sicredi.vote.adapter.dto.response.AssociateResponseDTO;
 import com.sicredi.vote.adapter.dto.response.TopicResponseDTO;
-import com.sicredi.vote.adapter.dto.response.VoteResponseDTO;
+import com.sicredi.vote.adapter.dto.response.TopicResultResponseDTO;
 import com.sicredi.vote.domain.service.TopicService;
 import com.sicredi.vote.infrastructure.exceptions.ErrorMessageDTO;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,12 +14,12 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -42,8 +43,9 @@ public class TopicController {
     }
 
     @Operation(summary = "Create a session", description = "Creates a session for a specified topic with a duration.")
-    @ApiResponse(responseCode = "201", description = "Session created successfully", content = @Content(schema = @Schema(implementation = CreateSessionRequestDTO.class)))
+    @ApiResponse(responseCode = "201", description = "Session created successfully")
     @ApiResponse(responseCode = "400", description = "Invalid data supplied", content = @Content(schema = @Schema(implementation = ErrorMessageDTO.class)))
+    @ApiResponse(responseCode = "404", description = "Topic not found", content = @Content(schema = @Schema(implementation = ErrorMessageDTO.class)))
     @PutMapping("/sessions/{id}")
     public ResponseEntity<Object> createSession(@Valid @RequestBody CreateSessionRequestDTO createSessionRequestDTO) {
         log.info("Attempting to create a session for topic ID: {} with duration: {} minutes", createSessionRequestDTO.getTopicId(), createSessionRequestDTO.getSessionDurationMinutes());
@@ -54,21 +56,21 @@ public class TopicController {
     }
 
     @Operation(summary = "Find all topics", description = "Search all topics")
-    @ApiResponse(responseCode = "200", description = "Topic found successfully", content = @Content(schema = @Schema(implementation = VoteResponseDTO.class)))
-    @ApiResponse(responseCode = "400", description = "Invalid data supplied", content = @Content(schema = @Schema(implementation = ErrorMessageDTO.class)))
+    @ApiResponse(responseCode = "200", description = "Topic found successfully", content = @Content(schema = @Schema(implementation = TopicResponseDTO.class)))
     @GetMapping
     public ResponseEntity<Object> findAll() {
         log.info("Attempting to find all topics");
 
         var result = this.topicService.findAll();
         log.info("Topics found successfully");
-        return ResponseEntity.ok(result);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     @Operation(summary = "Register a vote", description = "Register a vote by an associate on a specific topic.")
-    @ApiResponse(responseCode = "201", description = "Vote registered successfully", content = @Content(schema = @Schema(implementation = VoteResponseDTO.class)))
+    @ApiResponse(responseCode = "200", description = "Vote registered successfully")
     @ApiResponse(responseCode = "400", description = "Invalid data supplied", content = @Content(schema = @Schema(implementation = ErrorMessageDTO.class)))
-    @ApiResponse(responseCode = "404", description = "Session not valid", content = @Content(schema = @Schema(implementation = ErrorMessageDTO.class)))
+    @ApiResponse(responseCode = "404", description = "Topic or Associate not found", content = @Content(schema = @Schema(implementation = ErrorMessageDTO.class)))
+    @ApiResponse(responseCode = "500", description = "Error during voting process", content = @Content(schema = @Schema(implementation = ErrorMessageDTO.class)))
     @PutMapping("/vote")
     public ResponseEntity<Object> registerVote(@Valid @RequestBody VoteRequestDTO voteRequestDTO) {
         log.info("Attempting to register vote for topic ID: {}", voteRequestDTO.getTopicId());
@@ -78,10 +80,16 @@ public class TopicController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @Operation(summary = "Result votes", description = "Return results of votes by topic")
+    @ApiResponse(responseCode = "200", description = "Result votes generated successfully", content = @Content(schema = @Schema(implementation = TopicResultResponseDTO.class)))
+    @ApiResponse(responseCode = "400", description = "Invalid data supplied", content = @Content(schema = @Schema(implementation = ErrorMessageDTO.class)))
+    @ApiResponse(responseCode = "404", description = "Topic not found", content = @Content(schema = @Schema(implementation = ErrorMessageDTO.class)))
     @PostMapping("/resultVote")
-    public ResponseEntity<Object> resultVote(@RequestBody TopicResultRequestDTO topicResultRequestDTO) {
+    public ResponseEntity<Object> resultVote(@Valid @RequestBody TopicResultRequestDTO topicResultRequestDTO) {
+        log.info("Attempting result votes for topic ID: {}", topicResultRequestDTO.getTopicId());
 
-        var result = this.topicService.generateResult(topicResultRequestDTO.getId());
+        var result = this.topicService.generateResult(topicResultRequestDTO.getTopicId());
+        log.info("Result votes successfully");
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
